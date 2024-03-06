4e88bfd04356a0960f0abbd545626a4222bc2995
Girraj singhal <72375025+girraj12@users.noreply.github.com>
2024-03-06 11:31:11 +0530

    Add files via upload

---
+++ database/db.js
@@ -0,0 +1,13 @@
+import mongoose  from 'mongoose';
+
+const Connection = async (username = 'username', password = 'coder') => {
+   const URL = process.env.DATABASE
+    try {
+         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {   
+        console.log('Error while connecting with the database ', error);
+    }
+}
+
+export default Connection;
